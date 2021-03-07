4364aeb305f06ee027ffe5fa60aba1eec907d9b0
Aglicheev Mikhail <Toba.ru@mail.ru>
2021-03-07 18:38:06 +0300

    Added basic authentification

--- server_main.go
+++ server_main.go
@@ -1,13 +1,132 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
 )
 
+type user struct {
+	username     string
+	passwordHash string
+	firstName    string
+	lastName     string
+	avatar       string // path to avatar
+}
+
+type UserInput struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"passwordHash"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Avatar       string `json:"avatar"`
+}
+
+type usersMap struct {
+	users          map[int]user
+	lastFreeUserID int
+	mu             sync.Mutex
+}
+
+var users usersMap
+
+func (users *usersMap) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+
+	newUserInput := new(UserInput)
+	err := decoder.Decode(newUserInput)
+	if err != nil {
+		log.Printf("error while unmarshalling JSON: %s", err)
+		w.Write([]byte(`{"code": 400}`))
+		return
+	}
+
+	users.mu.Lock()
+
+	// TODO: Check if these fields are empty
+	users.users[users.lastFreeUserID] = user{
+		username:     newUserInput.Username,
+		passwordHash: newUserInput.PasswordHash,
+		firstName:    newUserInput.FirstName,
+		lastName:     newUserInput.LastName,
+		avatar:       newUserInput.Avatar,
+	}
+	users.lastFreeUserID++
+
+	users.mu.Unlock()
+
+	w.Write([]byte(`{"code": 201}`)) // returning success code
+}
+
+func (users *usersMap) HandleLoginUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+
+	newUserInput := new(UserInput)
+	err := decoder.Decode(newUserInput)
+	if err != nil {
+		log.Printf("error while unmarshalling JSON: %s", err)
+		w.Write([]byte(`{"code": 400}`))
+		return
+	}
+
+	for _, user := range users.users {
+		if user.username == newUserInput.Username {
+			if user.passwordHash != newUserInput.PasswordHash {
+				w.Write([]byte(`{"code": 400}`))
+				return
+			}
+			w.Write([]byte(`{"code": 200, "X-Expires-After": "Expires: Mon, 29 Mar 2021 10:00:00 GMT"}`)) // TODO: normal datetime
+			return
+		}
+	}
+
+	w.Write([]byte(`{"code": 400}`)) // No users with passed username
+	return
+}
+
+func (users *usersMap) HandleLogoutUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	//TODO: session handling
+	w.Write([]byte(`{"code": 400}`))
+	return
+}
+
 func authHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("{}"))
-	// TODO /auth handling
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Println(r.URL.Path)
+	switch r.URL.Path {
+	case "/auth/signup":
+		if r.Method != http.MethodPost {
+			w.Write([]byte(`{"code": 400}`))
+			return
+		}
+		users.HandleCreateUser(w, r)
+
+	case "/auth/login":
+		if r.Method != http.MethodGet {
+			w.Write([]byte(`{"code": 400}`))
+			return
+		}
+		users.HandleLoginUser(w, r)
+
+	case "/auth/logout":
+		if r.Method != http.MethodGet {
+			w.Write([]byte(`{"code": 400}`))
+			return
+		}
+		users.HandleLogoutUser(w, r)
+
+	default:
+		w.Write([]byte(`{"code": 400}`))
+		return
+	}
 }
 
 func pinHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +145,7 @@ func profileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func runServer(addr string) {
+	users = usersMap{users: make(map[int]user), lastFreeUserID: 0}
 	mux := http.NewServeMux()
 
 	server := http.Server{
@@ -34,12 +154,12 @@ func runServer(addr string) {
 	}
 
 	mux.HandleFunc("/auth/", authHandler)
+	mux.HandleFunc("/profile/", profileHandler)
 
 	mux.HandleFunc("/pin/", pinHandler)
 	mux.HandleFunc("/pins/", pinHandler)
 
 	mux.HandleFunc("/board/", boardHandler)
-	mux.HandleFunc("/profile/", profileHandler)
 
 	fmt.Printf("Starting server at localhost%s\n", addr)
 	server.ListenAndServe()
